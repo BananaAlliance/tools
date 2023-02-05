0283e46cfd53b778fcc1291d03ec2f570c6661c1
wladie <wladie.miller.dodo@gmail.com>
2023-02-05 15:16:13 +0300

    upd

--- logo.sh
+++ logo.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-function colors {
-  YELLOW="\e[132m"
-  BLACK="\e[139m"
-  ENDCOLOR="\e[0m"
-}
+YELLOW="\e[132m"
+BLACK="\e[139m"
+ENDCOLOR="\e[0m"
 
-colors
-
-echo -e "${YELLOW}######                                           #                                               ${NORMAL}"
-echo -e "${YELLOW}#     #   ##   #    #   ##   #    #   ##        # #   #      #      #   ##   #    #  ####  ######${NORMAL}"
-echo -e "${YELLOW}#     #  #  #  ##   #  #  #  ##   #  #  #      #   #  #      #      #  #  #  ##   # #    # #     ${NORMAL}"
-echo -e "${YELLOW}######  #    # # #  # #    # # #  # #    #    #     # #      #      # #    # # #  # #      ##### ${NORMAL}"
-echo -e "${YELLOW}#     # ###### #  # # ###### #  # # ######    ####### #      #      # ###### #  # # #      #     ${NORMAL}"
-echo -e "${YELLOW}#     # #    # #   ## #    # #   ## #    #    #     # #      #      # #    # #   ## #    # #     ${NORMAL}"
-echo -e "${YELLOW}######  #    # #    # #    # #    # #    #    #     # ###### ###### # #    # #    #  ####  ######${NORMAL}"
+echo -e "${YELLOW}######                                           #                                               ${ENDCOLOR}"
+echo -e "${YELLOW}#     #   ##   #    #   ##   #    #   ##        # #   #      #      #   ##   #    #  ####  ######${ENDCOLOR}"
+echo -e "${YELLOW}#     #  #  #  ##   #  #  #  ##   #  #  #      #   #  #      #      #  #  #  ##   # #    # #     ${ENDCOLOR}"
+echo -e "${YELLOW}######  #    # # #  # #    # # #  # #    #    #     # #      #      # #    # # #  # #      ##### ${ENDCOLOR}"
+echo -e "${YELLOW}#     # ###### #  # # ###### #  # # ######    ####### #      #      # ###### #  # # #      #     ${ENDCOLOR}"
+echo -e "${YELLOW}#     # #    # #   ## #    # #   ## #    #    #     # #      #      # #    # #   ## #    # #     ${ENDCOLOR}"
+echo -e "${YELLOW}######  #    # #    # #    # #    # #    #    #     # ###### ###### # #    # #    #  ####  ######${ENDCOLOR}"
