1b7cec84d368b4278766089177f399e3237b7e15
wladie <wladie.miller.dodo@gmail.com>
2023-02-16 20:14:34 +0300

    upd

--- logo.sh
+++ logo.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+function colors {
 YELLOW="\e[33m"
 BLACK="\e[30m"
 ENDCOLOR="\e[0m"
+}
 
 echo -e "${YELLOW}######                                           #                                               ${ENDCOLOR}"
 echo -e "${YELLOW}#     #   ##   #    #   ##   #    #   ##        # #   #      #      #   ##   #    #  ####  ######${ENDCOLOR}"
